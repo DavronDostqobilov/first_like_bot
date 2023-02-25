a2abf82dbd5f09898050a90b0ad4c6cead614686
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2023-02-25 13:35:31 +0500

    like bot data_base

--- bot.py
+++ bot.py
@@ -1,49 +1,61 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
 import os
+import json
 
 TOKEN=os.environ.get('TOKEN')
-i=0
-j=0
 
 updater = Updater(TOKEN)
 dp = updater.dispatcher
 
 def start(update: Update, context:CallbackContext):
     bot = context.bot
-
     chat_id = update.message.chat.id
 
-    btn1 = KeyboardButton(text=f"👍{i}")
-    btn2 = KeyboardButton(text=f"👎{j}")
 
+    try:
+        with open('data.json','r') as f:
+            data=json.loads(f.read())
+        like = data[chat_id]['like']
+        dislike = data[chat_id]['dislike']
+    except:
+        data={chat_id:{'like':0,'dislike':0}}
+        with open('data.json','w') as f:
+            json.dump(data,fp=f, indent=4)
+        
+    
+    btn1 = KeyboardButton(text=f"👍{like}")
+    btn2 = KeyboardButton(text=f"👎{dislike}")
     keyboard = ReplyKeyboardMarkup([[btn1, btn2]], resize_keyboard=True)
+    bot.sendMessage(chat_id, 'Like and Dislike', reply_markup=keyboard)
 
 
-    bot.sendMessage(chat_id, "LIKE and DISLIKE", reply_markup=keyboard)
-
-def like_and_dislike(update: Update, context: CallbackContext):
-    text = update.message.text
+def like_and_dislike(update: Update, context:CallbackContext):
     bot = context.bot
+    text = update.message.text
     chat_id = update.message.chat.id
-    global i
-    global j
+    with open('data.json','r') as f:
+        data=json.loads(f.read())
+    like = data[chat_id]['like']
+    dislike = data[chat_id]['dislike']        
     if text[0]=='👍':
-        i+=1
+        like+=1
 
     if text[0]=='👎':
-        j+=1
+        dislike+=1
+    data[chat_id]['like'] = like
+    data[chat_id]['dislike'] = dislike
+    with open('data.json','w') as f:
+        json.dump(data,fp=f, indent=4)
 
-    btn1 = KeyboardButton(text=f"👍{i}")
-    btn2 = KeyboardButton(text=f"👎{j}")
+    btn1 = KeyboardButton(text=f"👍{like}")
+    btn2 = KeyboardButton(text=f"👎{dislike}")
 
     keyboard = ReplyKeyboardMarkup([[btn1, btn2]], resize_keyboard=True)
-
-    bot.sendMessage(chat_id, "salom" , reply_markup=keyboard)
-
-        
-
-
+    bot.sendMessage(chat_id,text,reply_markup=keyboard)
+    
+updater=Updater(TOKEN)
+dp = updater.dispatcher
 dp.add_handler(CommandHandler('start', start))
 dp.add_handler(MessageHandler(Filters.text, like_and_dislike))
 
